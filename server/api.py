1b93ae66a9e87ae0092f00b53a7924a911d975a1
stan-is-laus <stanguldemond@gmail.com>
2018-09-24 10:11:59 +0200

    gitignore updated to exclude PyCharm stuff

--- server/api.py
+++ server/api.py
@@ -1,9 +1,32 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request, Response, json, jsonify, session, Session
 
-@app.route("/")
+app = Flask(__name__)
+sess = Session()
+
+@app.route('/')
 def hello():
     return "Hello Fieldlab!"
 
+@app.route('/init_disclosure', methods=['POST'])
+def init_disclosure_request():
+    data = request.get_data()
+
+    session['key'] = 'value'
+    return jsonify({"message": "OK"})
+
+@app.route('/get')
+def get_session():
+    return session.get('key', 'not set')
+
+def json_response(data):
+    response = Response(
+        response=json.dumps(data),
+        status=200,
+        mimetype='application/json',
+    )
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    return response
+
 if __name__ == '__main__':
+    sess.init_app(app)
     app.run()
