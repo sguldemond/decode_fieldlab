a23114939a1031b3dcd79d2d7dd80f8844936fc4
stan-is-laus <stanguldemond@gmail.com>
2018-09-25 16:02:11 +0200

    auto deploy

--- server/api.py
+++ server/api.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, Response, json, session, send_from_directory
 from flask_cors import CORS
+import random
+
 import session_manager
 import data_source
 import validator
@@ -61,12 +63,26 @@ def accept_request():
     attribute_request = session_manager.get_session(session_id)['request']
     validator_response = validator.check(attribute_request, brp_data)
 
-    active_session = session_manager.append_session_data(session_id, {'request_valid': validator_response, 'request_status': 'ACCEPTED'})
+    random_color = "rgb({0},{1},{2})".format(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+    data = {'request_valid': validator_response, 'request_status': 'ACCEPTED', 'secret': random_color}
+    active_session = session_manager.append_session_data(session_id, data)
 
     # can not be ended here, since requestor needs to access data still
     # session_manager.end_session(session_id)
 
     return json_response({'response': active_session})
+
+
+@app.route('/get_picture_url', methods=['POST'])
+def get_picture_url():
+    data = request.get_data()
+    data_json = json.loads(data)
+    username = data_json['username']
+
+    user_picture_url = data_source.get_picture_url(username)
+
+    return json_response({'response': user_picture_url})
 
 
 @app.route('/deny_request', methods=['POST'])
--- server/data_source.py
+++ server/data_source.py
@@ -1,8 +1,9 @@
 brp_data = []
+picture_default_url = "https://raw.githubusercontent.com/sguldemond/decode_fieldlab/development/files/default-man.png"
 
 def generate_dummy_data():
-    data_person1 = {'BSN': '830563313', 'surname': 'Jansen', 'initials': 'AJ', 'dateOfBirth': '02-08-1999'}
-    data_person2 = {'BSN': '277345108', 'surname': 'Pietersen', 'initials': 'PWA', 'dateOfBirth': '24-12-2001'}
+    data_person1 = {'BSN': '830563313', 'surname': 'Jansen', 'initials': 'AJ', 'dateOfBirth': '02-08-1999', 'picture': picture_default_url}
+    data_person2 = {'BSN': '277345108', 'surname': 'Pietersen', 'initials': 'PWA', 'dateOfBirth': '24-12-2001', 'picture': picture_default_url}
 
     account_person1 = {'username': 'aj.jansen', 'password': 'foobar'}
     account_person2 = {'username': 'p.pietersen', 'password': 'foobar'}
@@ -16,3 +17,10 @@ def get_data(username):
     for data in brp_data:
         if data['account']['username'] == username:
             return data['data']
+
+
+def get_picture_url(username):
+    for data in brp_data:
+        if data['account']['username'] == username:
+            return data['data']['picture']
+
