fe7f1d2608b218fd0295e8fc0c4c8ecd3568dbb6
stan-is-laus <stanguldemond@gmail.com>
2018-09-25 15:11:59 +0200

    shell scripts removed

--- connect.vps.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-ssh ubuntu@185.54.115.75 -i ~/.ssh/id_rsa
-
-
-#scp -r <folder> ubuntu@185.54.115.75:<folder>
--- deploy.vps.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-git add .
-git commit -m "auto deploy"
-git push origin development
-
-# scp -r server ssh decodeams@oscity.nl:~/decode_docker/decode_fieldlab/server
-# scp -r server ubuntu@185.54.115.75:~/
